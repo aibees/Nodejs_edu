497e3936bbe5f9d8f790628dbb51b98a7da980e1
aibees <aibees1129@naver.com>
2019-01-20 16:27:38 +0900

    sequelize 'find' function modity

--- apis/userapi.js
+++ apis/userapi.js
@@ -2,7 +2,7 @@ const {User} = require('../models');
 
 exports.userget = function(req, res) {
     console.log("user get");
-    User.findAll({})
+    User.findAll({row : true})
     .then(users => res.send(users))
     .catch(err => res.status(500).send(err));
 };
@@ -10,40 +10,64 @@ exports.userget = function(req, res) {
 exports.usergetbyid = function(req, res) {
     console.log("user get by id");
     let query = req.params.id;
+    User.findAll({
+        where : {
+            ID : query
+        }
+    })
+    .then(user => res.send(user))
+    .catch(err => res.status(500).send(err));
     console.log(query);
 };
 
 exports.userinsert = function(req, res) {
     console.log('user insert');
-    User.create({
-        nickname : req.body.nickname,
-        ID : req.body.ID,
-        password : req.body.password,
-        email : req.body.email
+    User.findOrCreate({
+        where : {nickname : req.body.nickname},
+        defaults : {
+            nickname : req.body.nickname,
+            ID : req.body.ID,
+            password : req.body.password,
+            email : req.body.email
+        }
     })
-    .then(() => res.send("insert success"))
+    .spread((result, created) => {
+        if(created) {
+            console.log(results);
+            res.send("insert success");
+        }
+        else {
+            res.send("already exist");
+        }
+    })
     .catch(err => res.status(500).send(err));
 }
 
 exports.userupdate = function(req, res) {
     console.log('user update');
-    let query = req.params.id;
-    const user = {
-        'num' : req.body.num,
-        'id' : req.body.id,
-        'pwd' : req.body.pwd
-    };
+    User.update({
+            nickname : req.body.nickname,
+            ID : req.body.ID,
+            password : req.body.password,
+            email : req.body.email
+        }, {
+            where : {
+                ID : req.body.ID
+            }
+        }
+    )
+    .then(() => res.send("update success"))
+    .catch(err => res.status(500).send(err));
 }
 
 exports.userdelete = function(req, res) {
     console.log('user delete');
     let query = req.params.id;
-   /*  connect.query('delete from user where id = ?', [query], (err, row, fields) => {
-        if(err)
-            console.log(err);
-        else {
-            console.log('user delete');
-            res.send('delete success');
+    User.destroy({
+        where : {
+            ID : query
         }
-    }) */
+    })
+    .then(() => res.send("delete success"))
+    .catch(err => res.status(500).send(err));
 }
