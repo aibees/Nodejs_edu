f55260b06866212e4c60ebb10eef09ee822caa3e
aibees <aibees1129@naver.com>
2019-01-24 02:32:33 +0900

    helmet module add

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+//module definition
 const express = require('express');
 const session = require('express-session');
 const fileStore = require('session-file-store')(session);
+const helmet = require('helmet');
 
+//variable create
 let body = require('body-parser');
 let app = express();
 
@@ -11,11 +14,17 @@ app.set('views', './views');
 
 //session
 app.use(session({
+    name : 'aibeesSession',
     secret : 'keyboard cat', //session 암호화
     resave : true, //session을 항상 저장할 지 여부를 정하는 값
     saveUninitialized : true, //세션 아이디를 실제 사용하기 전에는 발급하지 않음
     store : new fileStore()
 }));
+
+//security
+    //helmet
+app.use(helmet());
+app.disable('x-powered-by');
 
 //body module add
 app.use(body.urlencoded({extended:true}));
@@ -26,6 +35,7 @@ app.use('/user', require('./routers/user'));
 app.use('/auth', require('./routers/auth'));
 app.use('/session', require('./routers/session'));
 
+//listen
 app.listen(3000, 'localhost', () => {
     console.log('Server On');
     require('./models').sequelize.sync({force : false})
